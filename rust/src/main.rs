a5efe61afac342c6c5633115fc36f3021f600906
Micah Resnick <resnickmicah@gmail.com>
2024-10-06 13:25:20 -0400

    Remove serde rename

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,7 +3,8 @@ use reqwest::header;
 // function found using https://curlconverter.com/rust/
 // create a global constant
 static USER_AGENT: &str = "getMyCollection/0.1 +http://localhost";
-static BASE_URL: &str = "https://api.discogs.com/users/Ospreythirtyone/collection/folders/0/releases";
+static BASE_URL: &str =
+    "https://api.discogs.com/users/Ospreythirtyone/collection/folders/0/releases";
 
 fn initial_request() -> Result<String, Box<dyn std::error::Error>> {
     let mut headers = header::HeaderMap::new();
@@ -13,17 +14,13 @@ fn initial_request() -> Result<String, Box<dyn std::error::Error>> {
         .redirect(reqwest::redirect::Policy::none())
         .build()
         .unwrap();
-    let res: String = client.get(BASE_URL)
-        .headers(headers)
-        .send()?
-        .text()?;
+    let res: String = client.get(BASE_URL).headers(headers).send()?.text()?;
     Ok(res)
 }
-
 
 fn main() {
     match initial_request() {
         Ok(res) => println!("{}", res),
-        Err(err) => eprintln!("{}", err)
+        Err(err) => eprintln!("{}", err),
     }
 }
