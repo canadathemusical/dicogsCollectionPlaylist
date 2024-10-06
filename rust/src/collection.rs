ef6083bdab05fef50ecdf6f18332d663deefa50b
daniel <ospreythirtyone@protonmail.com>
2024-10-06 15:53:06 -0400

    cleanup

--- rust/src/collection.rs
+++ rust/src/collection.rs
@@ -92,6 +92,34 @@ pub struct Label {
     pub resource_url: String,
 }
 
+fn clean_artist_name(artist: String) -> String {
+    let regex = Regex::new(r"\s*\(\d+\)\s*").unwrap();
+    let result = regex.replace_all(artist.as_str(), "");
+        return result.to_string();
+}
+
+fn format_release(release: Album) -> String {
+    let title: String = release.basic_information.title;
+    let artist: String = release
+        .basic_information
+        .artists
+        .iter()
+        .map(|artist: &Artist| clean_artist_name(artist.name.clone()))
+        .collect::<Vec<_>>()
+        .join(" & ");
+
+    format!("{} - {}", artist, title)
+}
+
+fn get_random_releases(releases: Vec<Album>) -> Vec<Album> {
+    let mut random_releases: Vec<Album> = Vec::new();
+    for _ in 0..4 {
+        let random_index = rand::thread_rng().gen_range(0..releases.len());
+        random_releases.push(releases[random_index].clone());
+    }
+    random_releases
+}
+
 pub fn get_all_releases() -> Result<Vec<Album>, Box<dyn std::error::Error>> {
     let mut headers = header::HeaderMap::new();
     headers.insert("User-Agent", USER_AGENT.parse().unwrap());
@@ -125,53 +153,11 @@ pub fn get_all_releases() -> Result<Vec<Album>, Box<dyn std::error::Error>> {
     Ok(all_releases)
 }
 
-impl std::fmt::Display for Album {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(
-            f,
-            "Title: {}, Year: {}",
-            self.basic_information.title, self.basic_information.year
-        )
-    }
-}
-
-// create a function to strip artist names of /\(\d+\)/
-fn clean_artist_name(artist: String) -> String {
-    let regex = Regex::new(r"\s*\(.*?\)\s*").unwrap();
-    let result = regex.replace_all(artist.as_str(), "");
-        return result.to_string();
-}
-
-pub fn format_release(release: Album) -> String {
-    let title: String = release.basic_information.title;
-    let artist: String = release
-        .basic_information
-        .artists
-        .iter()
-        .map(|artist: &Artist| clean_artist_name(artist.name.clone()))
-        .collect::<Vec<_>>()
-        .join(" & ");
-
-    format!("{} - {}", artist, title)
-}
-
-// pub fn formatted_releases(releases: Vec<Album>) -> Vec<String> {
-//     let mut formatted_releases: Vec<String> = Vec::new();
-
-//     for release in releases {
-//         formatted_releases.push(format_release(release));
-//     }
-//     formatted_releases
-// }
-
 pub fn print_releases(releases: Vec<Album>) -> String {
     let mut output = String::new();
-    // create a vec of 4 random releases
-    let mut random_releases: Vec<Album> = Vec::new();
-    for _ in 0..4 {
-        let random_index = rand::thread_rng().gen_range(0..releases.len());
-        random_releases.push(releases[random_index].clone());
-    }
+    
+    let random_releases: Vec<Album> = get_random_releases(releases);
+
     for release in random_releases {
         let release = format_release(release);
         output.push_str(&format!("{:?}\n", release));
