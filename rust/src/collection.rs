aa50f3d6d81f39f0ea9d1e8b5a85b24dc8db8d07
daniel <ospreythirtyone@protonmail.com>
2024-10-06 13:27:38 -0400

    CollectionReleasesResponse struct

--- rust/src/collection.rs
+++ rust/src/collection.rs
@@ -3,7 +3,7 @@ use serde_derive::Serialize;
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Root {
+pub struct CollectionReleasesResponse {
     pub pagination: Pagination,
     pub collection: Collection,
 }
